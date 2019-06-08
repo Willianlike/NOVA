9ab637be31af8363d241ca8b3890473a06707e07
asterium-ios <ios@asterium.ru>
2019-06-08 14:52:03 +0300

    topbar

---
+++ Nova/Components/RoundButton.swift
@@ -0,0 +1,9 @@
+//
+//  RoundButton.swift
+//  Nova
+//
+//  Created by Ilia Yurasov on 08/06/2019.
+//  Copyright © 2019 Вильян Яумбаев. All rights reserved.
+//
+
+import Foundation
---
+++ Nova/Components/RoundView.swift
@@ -0,0 +1,9 @@
+//
+//  RoundView.swift
+//  Nova
+//
+//  Created by Ilia Yurasov on 08/06/2019.
+//  Copyright © 2019 Вильян Яумбаев. All rights reserved.
+//
+
+import Foundation
