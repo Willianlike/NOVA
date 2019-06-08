7bf244d1e7f95ea8e1aaf66ce5efca111cac612d
asterium-ios <ios@asterium.ru>
2019-06-08 20:07:08 +0300

    test vc

---
+++ Nova/Components/TestVC.swift
@@ -0,0 +1,19 @@
+//
+//  TestVC.swift
+//  Nova
+//
+//  Created by Ilia Yurasov on 08/06/2019.
+//  Copyright © 2019 Вильян Яумбаев. All rights reserved.
+//
+
+
+import UIKit
+import RxSwift
+import RxDataSources
+import RxCocoa
+import Cartography
+
+class TestVC: BaseVC {
+
+
+}
