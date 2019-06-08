2296959f6d6facf710c3a0c6fd5539ed457b865c
asterium-ios <ios@asterium.ru>
2019-06-08 16:36:14 +0300

    topbar 

--- Nova/Components/NovaTopBar.swift
+++ Nova/Components/NovaTopBar.swift
@@ -14,6 +14,27 @@ class NovaTopBar: UIView {
     @IBOutlet weak var backButton: RoundButton!
     @IBOutlet weak var progressConstraint: NSLayoutConstraint!
     @IBOutlet weak var stageTitle: UILabel!
-    @IBOutlet weak var stageDescription: UILabel!
+
+    let nibName = "NovaTopBar"
+
+    @IBOutlet var content: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    func commonInit() {
+        let bundle = Bundle(for: NovaTopBar.self)
+        bundle.loadNibNamed(nibName, owner: self, options: nil)
+        addSubview(content)
+        content.frame = self.bounds
+        content.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
 
 }
