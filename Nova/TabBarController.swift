0c8914a6df910dd722e417f32339918d1234739e
Willian <vilyanyaumbaev@gmail.com>
2019-06-08 11:19:34 +0300

    added tabbar

---
+++ Nova/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  Nova
+//
+//  Created by Вильян Яумбаев on 08/06/2019.
+//  Copyright © 2019 Вильян Яумбаев. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    
+    
+    
+}
