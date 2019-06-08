2a126a41f9d044c14f441a0681d6e639b8ae7669
asterium-ios <ios@asterium.ru>
2019-06-08 14:52:48 +0300

    topbar

--- Nova/Components/NovaTopBar.swift
+++ Nova/Components/NovaTopBar.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+
+class NovaTopBar: UIView {
+
+    @IBOutlet weak var backButton: RoundButton!
+    @IBOutlet weak var progressConstraint: NSLayoutConstraint!
+    @IBOutlet weak var stageTitle: UILabel!
+    @IBOutlet weak var stageDescription: UILabel!
+
+}
--- Nova/Components/RoundButton.swift
+++ Nova/Components/RoundButton.swift
@@ -7,3 +7,108 @@
 //
 
 import Foundation
+import UIKit
+
+var disabledColorHandle: UInt8 = 0
+var highlightedColorHandle: UInt8 = 0
+var selectedColorHandle: UInt8 = 0
+
+@IBDesignable class RoundButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+
+    private func image(withColor color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        self.setBackgroundImage(image(withColor: color), for: state)
+    }
+
+    @IBInspectable
+    var disabledColor: UIColor? {
+        get {
+            if let color = objc_getAssociatedObject(self, &disabledColorHandle) as? UIColor {
+                return color
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                self.setBackgroundColor(color, for: .disabled)
+                objc_setAssociatedObject(self, &disabledColorHandle, color, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            } else {
+                self.setBackgroundImage(nil, for: .disabled)
+                objc_setAssociatedObject(self, &disabledColorHandle, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var masksToBounds: Bool {
+        set {
+            layer.masksToBounds = newValue
+        }
+        get {
+            return layer.masksToBounds
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
